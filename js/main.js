b706b91163c45a4d6e01e00b5dc431b8697ba07b
ewerton336 <65933715+ewerton336@users.noreply.github.com>
2020-12-14 23:53:23 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*
+var nome = "Ewerton";
+alert("Olá " + nome.toUpperCase());
+var idade1 = 13;
+var idade2 = 23;
+var idade3 = idade1+idade2;
+console.log (idade3);
+*/
+
+/*
+var lista = ["ovo", "laranja", "queijo"];
+lista.push("maionese");
+alert(lista.join(" | "));
+*/
+
+/*
+var idade = prompt("Qual sua idade?")
+
+if (idade >= 18){
+    alert("maior de idade")
+}
+else {
+    alert ("menor de idade")
+}
+*/
+
+function ao_clicar() {
+    alert("Em desenvolvimento")
+}
+
+function alterar_elemento(){
+    document.getElementById("agradecimento").innerHTML = "ooiiii";
+
+}
+
+function retorna_elemento(){
+    document.getElementById("agradecimento").innerHTML = "a";
+}
