7b1182784492b5ae1ca2d0cbf997f1c5d7643747
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-02-17 16:02:41 -0300

    Add files via upload
    Atualizado botões e funções javascript. 
    
    Criar:
    Inserção de caixa de texto com input caso o tipo de produto "Outros" seja selecionado

--- js/main.js
+++ js/main.js
@@ -1,46 +1,24 @@
-/*
-var nome = "Ewerton";
-alert("Olá " + nome.toUpperCase());
-var idade1 = 13;
-var idade2 = 23;
-var idade3 = idade1+idade2;
-console.log (idade3);
-*/
 
-/*
-var lista = ["ovo", "laranja", "queijo"];
-lista.push("maionese");
-alert(lista.join(" | "));
-*/
-
-/*
-var idade = prompt("Qual sua idade?")
-
-if (idade >= 18){
-    alert("maior de idade")
-}
-else {
-    alert ("menor de idade")
-}
-*/
 
 function ao_clicar() {
     alert("Em desenvolvimento")
 }
 
-function alterar_elemento(){
+function alterar_elemento() {
     document.getElementById("agradecimento").innerHTML = "ooiiii";
 
 }
 
-function retorna_elemento(){
+function retorna_elemento() {
     document.getElementById("agradecimento").innerHTML = "a";
 }
 
-function yesnoCheck(that) {
-    if (that.value == "other") {
-        document.getElementById("ifYes").style.display = "block";
-    } else {
-        document.getElementById("ifYes").style.display = "none";
-    }
-}
+// function yesnoCheck(that) {
+//     if (that.value == "other") {
+//         document.getElementById("ifYes").style.display = "block";
+//     } else {
+//         document.getElementById("ifYes").style.display = "none";
+//     }
+// }
+
+
